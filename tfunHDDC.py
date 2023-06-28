e46ffd87fa7a15c8fdf1a7863b06ca03a9672ffb
Malcolm Nielsen <malcolmgjpnielsen@hotmail.ca>
2023-06-28 14:18:32 -0600

    Update tyxf7 and tyxf8 functions

--- tfunHDDC.py
+++ tfunHDDC.py
@@ -691,73 +691,75 @@ def _T_funhddt_m_step1(fdobj, Wlist, K, t, tw, nux, dfupdate, dfconstr, model,
 # *
 # */
 def _T_tyxf7(dfconstr, nux, n, t, tw, K, p, N):
+    newnux = np.zeros(3)
     if dfconstr == "no":
-        dfoldg = nux
+        dfoldg = nux.copy()
 
         #scipy digamma is slow? https://gist.github.com/timvieira/656d9c74ac5f82f596921aa20ecb6cc8
         for i in range(0, K):
             constn = 1 + (1/n[i]) * np.sum(t[:, i] * (np.log(tw[:, i]) - tw[:, i])) + scip.digamma((dfoldg[i] + p)/2) - np.log((dfoldg[i] + p)/2)
-
+            temp = scip.digamma((dfoldg[i] + p)/2)
+            
             f = lambda v : np.log(v/2) - scip.digamma(v/2) + constn
-            #Verify this outputs the same as R: may need to set rtol to 0
-            nux[i] = scio.brentq(f, 0.0001, 1000, xtol=0.00001)
+            newnux[i] = scio.brentq(f, 0.0001, 1000, xtol=0.00001)
 
-            if nux[i] > 200:
-                nux[i] = 200
+            if newnux[i] > 200:
+                newnux[i] = 200.
 
-            if nux[i] < 2:
-                nux[i] = 2
+            if newnux[i] < 2:
+                newnux[i] = 2.
 
     else:
         dfoldg = nux[0]
         constn = 1 + (1/N) * np.sum(t *(np.log(tw) - tw)) + scip.digamma( (dfoldg + p) / 2) - np.log( (dfoldg + p) / 2)
 
+        print(constn)
         f = lambda v : np.log(v/2) - scip.digamma(v/2) + constn
-            #Verify this outputs the same as R: may need to set rtol to 0
-        dfsamenewg = scio.brentq(f, 0.0001, 1000, xtol=0.00001)
+        dfsamenewg = scio.brentq(f, 0.0001, 1000, xtol=0.01)
 
         if dfsamenewg > 200:
-            dfsamenewg = 200
+            dfsamenewg = 200.
         
         if dfsamenewg < 2:
-            dfsamenewg = 2
+            dfsamenewg = 2.
 
-        nux = np.repeat(dfsamenewg, K)
+        newnux = np.repeat(dfsamenewg, K)
 
-    return nux
+    return newnux
 
 def _T_tyxf8(dfconstr, nux, n, t, tw, K, p, N):
+    newnux = np.zeros(3)
+
     if(dfconstr == "no"):
-        dfoldg = nux
+        dfoldg = nux.copy()
         
         for i in range(0, K):
-            constn = 1 + (1 / n[i]) * np.sum(t[:, i] * np.log(tw[:, i] - tw[:, i])) + scip.digamma((dfoldg[i] + p)/2) - np.log( (dfoldg[i] + p)/2)
-
+            constn = 1 + (1 / n[i]) * np.sum(t[:, i] * (np.log(tw[:, i]) - tw[:, i])) + scip.digamma((dfoldg[i] + p)/2) - np.log( (dfoldg[i] + p)/2)
             constn = -constn
-            nux[i] = (-np.exp(constn) + 2 * (np.exp(constn)) * (np.exp(scip.digamma(dfoldg[i] / 2)) - ( (dfoldg[i]/2) - (1/2)))) / (1 - np.exp(constn))
+            newnux[i] = (-np.exp(constn) + 2 * (np.exp(constn)) * (np.exp(scip.digamma(dfoldg[i] / 2)) - ( (dfoldg[i]/2) - (1/2)))) / (1 - np.exp(constn))
 
-            if nux[i] > 200:
-                nux[i] = 200
+            if newnux[i] > 200:
+                newnux[i] = 200.
 
-            if nux[i] < 2:
-                nux[i] = 2
+            if newnux[i] < 2:
+                newnux[i] = 2.
 
     else:
         dfoldg = nux[0]
-        constn = 1 + (1 / n[i]) * np.sum(t[:, i] * np.log(tw[:, i] - tw[:, i])) + scip.digamma((dfoldg[i] + p)/2) - np.log( (dfoldg[i] + p)/2)
+        constn = 1 + (1 / N) * np.sum(t * (np.log(tw) - tw)) + scip.digamma((dfoldg + p)/2) - np.log( (dfoldg + p)/2)
         constn = -constn
 
-        dfsamenewg = (-np.exp(constn) + 2 * (np.exp(constn)) * (np.exp(scip.digamma(dfoldg[i] / 2)) - ( (dfoldg[i]/2) - (1/2)))) / (1 - np.exp(constn))
+        dfsamenewg = (-np.exp(constn) + 2 * (np.exp(constn)) * (np.exp(scip.digamma(dfoldg / 2)) - ( (dfoldg/2) - (1/2)))) / (1 - np.exp(constn))
 
         if dfsamenewg > 200:
-            dfsamenewg = 200
+            dfsamenewg = 200.
 
         if dfsamenewg < 2:
-            dfsamenewg = 2
+            dfsamenewg = 2.
 
-        nyx = np.repeat(dfsamenewg, K)
+        newnux = np.repeat(dfsamenewg, K)
 
-    return nux
+    return newnux
 
 def _T_imahalanobis(x, muk, wk, Qk, aki):
     
