80413de5da50a30c70fc33a28478dbc22be44933
Malcolm Nielsen <malcolmgjpnielsen@hotmail.ca>
2023-07-01 01:07:25 -0600

    add _T_hddc_ari function

--- tfunHDDC.py
+++ tfunHDDC.py
@@ -460,6 +460,22 @@ def _T_initmypca_fd1(fdobj, Wlist, Ti):
 
     return ipcafd
 
+def _T_hddc_ari(x, y):
+    if type(x) != np.ndarray:
+        x = np.array(x)
+
+    if type(y) != np.ndarray:
+        y = np.array(y)
+
+    tab = pd.crosstab(x, y)
+    if np.all(tab.shape == (1,1)): return 1
+    a = np.sum(scip.binom(tab, 2))
+    b = np.sum(scip.binom(np.sum(tab, axis=1), 2)) - a
+    c = np.sum(scip.binom(np.sum(tab, axis=0), 2)) - a
+    d = scip.binom(np.sum(tab), 2) - a - b - c
+    ari = (a - (a + b) * (a + c)/(a+b+c+d))/(a+b+a+c)/2 - (a+b) * (a + c)/(a+b+c+d))
+    return ari
+
 # Why not just pass in x instead of fdobj?
 def _T_funhddt_twinit(fdobj, wlist, par, nux):
 
