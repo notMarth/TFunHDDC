05ec9b9233227d783d23610c099e636aa8e7bd18
Malcolm Nielsen <malcolmgjpnielsen@hotmail.ca>
2023-06-30 23:57:21 -0600

    Add univariate _T_initmypca_fd1 function

--- tfunHDDC.py
+++ tfunHDDC.py
@@ -430,12 +430,41 @@ def _T_funhddc_main1(fdobj, wlist, K, dfstart, dfupdate, dfconstr, model,
 
         return params
         
-        
+def _T_initmypca_fd1(fdobj, Wlist, Ti):
+    
+    #TODO add multivariate
+    mean_fd = fdobj
+    #TODO does pass by reference happen here like with lists?
+    coef = fdobj.coefficients
+    #by default numpy cov function uses rows as variables and columns as observations, opposite to R
+    mat_cov = np.cor(m=coef, aweights=Ti, ddof=0, rowvar=False)
+    #may need to try this with other params depending on how weights are passed in
+    coefmean = np.average(coef, axis=0, weights=Ti)
+    #Verify this
+    fdobj.coefficients = np.apply_along_axis(lambda col: col - coefmean, axis=0, arr=fdobj.coefficients)
+
+    #Replaces as.matrix(data.frame(mean=coefmean))
+    mean_fd.coefficients = {'mean':coefmean}
+    cov = (Wlist['W_m']@mat_cov)@(Wlist['W_m'].T)
+
+    valeurs = np.linalg.eig(cov)
+    valeurs_propres = valeurs.eigenvalues
+    vecteurs_propres = valeurs.eigenvectors
+    fonctionspropres = fdobj
+    bj = np.linalg.solve(Wlist['W_m'], np.eye(Wlist['W_m'].shape[0]))@vecteurs_propres
+    fonctionspropres['coefficients'] = bj
+
+    scores = skfda.misc.inner_product_matrix(fdobj, fonctionspropres)
+    varprop = valeurs_propres / np.sum(valeurs_propres)
+    ipcafd = {'valeurs_propres': valeurs_propres, 'harmonic': fonctionspropres, 'scores': scores, 'covariance': cov, 'U':bj, 'meanfd': mean_fd, 'mux': coefmean}
+
+    return ipcafd
+
 # Why not just pass in x instead of fdobj?
 def _T_funhddt_twinit(fdobj, wlist, par, nux):
 
     #try this if fdobj is an fdata (Univariate only right now)
-    if(type(fdobj) == skfda.FDataBasis or type(fdobj == skfda.FDataGrid)):
+    if(type(fdobj) == skfda.FDataBasis or type(fdobj) == skfda.FDataGrid):
         x = fdobj.coefficients
 
     #For R testing if fdobj gets passed as a dict
