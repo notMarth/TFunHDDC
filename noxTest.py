609bc7dd73421c7a14fcdff94fe35f336f22c21a
Malcolm Nielsen <malcolmgjpnielsen@hotmail.ca>
2023-09-05 13:12:22 -0600

    Add changes to noxTest

--- noxTest.py
+++ noxTest.py
@@ -1,7 +1,25 @@
 import tfunHDDC as tfun
 import numpy as np
 import NOxBenchmark as NOx
+from sklearn import metrics as met
+import csv
 
-data = NOx.fitNOxBenchmark()['data']
+if __name__ == '__main__':
+    data = NOx.fitNOxBenchmark()
+    #NOx.plot_NOx(data)
 
-res = tfun.tfunHDDC(data)
+    vec = []
+    '''
+    with open('HWcluster.csv', newline='') as csvfile:
+        datareader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        for row in datareader:
+            vec.append(row[0].split(',')[1:][0])
+ 
+    vec = np.array(vec[1:], dtype=int)
+    vec = vec - 1
+    print(vec)
+    '''
+
+    res = tfun.tfunHDDC(data['data'], min_individuals=3, model='all', K=2, threshold=[0.05, 0.2, 0.4, 0.6], nb_rep=20, init='kmeans')
+    print(np.sum(np.diag(met.confusion_matrix(res.cl, data['labels'])))/len(data['labels']))
+    print(tfun._T_hddc_ari(data['labels'], res.cl))
