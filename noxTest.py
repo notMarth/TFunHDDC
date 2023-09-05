6bafd88a0d72a34f2139204919b6d3927b0e3fc7
popescuc71 <86089796+popescuc71@users.noreply.github.com>
2023-09-05 12:07:44 -0600

    changes for examples

--- noxTest.py
+++ noxTest.py
@@ -1,7 +1,10 @@
 import tfunHDDC as tfun
 import numpy as np
 import NOxBenchmark as NOx
-
-data = NOx.fitNOxBenchmark()['data']
-
-res = tfun.tfunHDDC(data)
+from sklearn import metrics as met
+data = NOx.fitNOxBenchmark(15)
+labels = data['labels']
+data = data['data']
+model1=["AkjBkQkDk", "AkjBQkDk", "AkBkQkDk", "ABkQkDk", "AkBQkDk", "ABQkDk"]
+res = tfun.tfunHDDC(data,k=2,threshold=0.6,model=model1, init="kmeans",nb_rep=20,dfconstr="no" ,min_individuals=3)
+print(met.confusion_matrix(res.cl, labels))
