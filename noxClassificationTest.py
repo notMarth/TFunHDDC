6cfb6003fe825e75e021d767120a803517faa5f5
Malcolm Nielsen <malcolmgjpnielsen@hotmail.ca>
2023-09-28 13:24:56 -0600

    Add classification tests

--- noxClassificationTest.py
+++ noxClassificationTest.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     results = {}
     for i in models:
         start = time.time()
-        res = tfun.tfunHDDC(data['data'][training], min_individuals=4, model=i, K=2, threshold=[0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6], nb_rep=20, init='kmeans', known = known, mc_cores=16)
+        res = tfun.tfunHDDC(data['data'][training], min_individuals=4, model=i, K=2, threshold=[0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6], nb_rep=20, init='kmeans', known = known)
         results[i] = [np.sum(np.diag(met.confusion_matrix(res.cl, known)))/len(known)]
         results[i].append(met.confusion_matrix(res.cl, known))
         
@@ -27,5 +27,5 @@ if __name__ == '__main__':
         results[i].append(met.confusion_matrix(pred['class'], labels[test]))
         results[i].append(tfun._T_hddc_ari(labels[test], pred['class']))
         
-    for i in results.values():
-        print(i)
+    for i in results:
+        print(f'{i} {results[i]}')
