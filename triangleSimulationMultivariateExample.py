69383c5a4895a7a1a2609660784fa0e2b0762979
Malcolm Nielsen <malcolmgjpnielsen@hotmail.ca>
2023-11-27 19:09:06 -0700

    Fix triangle simulation example

--- triangleSimulationMultivariateExample.py
+++ triangleSimulationMultivariateExample.py
@@ -6,8 +6,7 @@ from sklearn import metrics as met
 
 '''
 Multivariate example using an example from the paper which TFunHDDC is based on.
-Each model is run separately, as the log likelihood may prefer a clustering with
-a higher CCR but lower ARI.
+Run triangle simulation from TFunHDDC paper.
 
 After each model is run, the ARI and a table comparing the clustered classes vs
 the true labels are outputted.
@@ -19,11 +18,9 @@ if __name__ == '__main__':
     data = tri.genTriangleScenario1()
     fd = data['data']
     labels = data['labels']
-    models = ['akjbkqkdk', 'akjbqkdk', 'akbkqkdk', 'akbqkdk', 'abkqkdk', 'abqkdk']
 
+    tri.plotTriangles(data)
 
-    for i in models:
-        res = tfun.tfunHDDC(fd, K=6, model=i, threshold=[0.01, 0.05, 0.1, 0.2, 0.4, 0.6], nb_rep=20)
-        print(i)
-        print(met.confusion_matrix(labels, res.cl))
-        print(tfun._T_hddc_ari(labels, res.cl))
+    res = tfun.tfunHDDC(fd, K=6, model='all', threshold=0.2, nb_rep=20)
+    print(met.confusion_matrix(labels, res.cl))
+    print(tfun._T_hddc_ari(labels, res.cl))
